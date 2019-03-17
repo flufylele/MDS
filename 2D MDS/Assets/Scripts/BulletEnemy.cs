bb0853c90bb347c21bca66a6912910fdbe1f9486
flufylele <ghostdenis@yahoo.com>
2019-03-18 01:01:18 +0200

    Enemy AI
    Enemy moving towards player when in a specified range (also shoots at the player)

---
+++ 2D MDS/Assets/Scripts/BulletEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEnemy : MonoBehaviour
+{
+
+    public float moveSpeed = 7f;
+
+    Rigidbody2D rb;
+
+    public Transform target;
+    Vector2 moveDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        moveDirection = (target.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            Debug.Log("Hit!");
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ 2D MDS/Assets/Scripts/EnemySenses.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySenses : MonoBehaviour
+{
+
+    public float speed;
+    public float startingDistance;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    float fireRate;
+    float nextFire;
+
+    private Transform target;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        fireRate = 1f;
+        nextFire = Time.time;
+    }
+
+    private void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) < startingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {   
+        if(Time.time > nextFire)
+        {
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+        
+    }
+
+
+}
+
