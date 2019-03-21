6a50d4f15d40fd396a841f5cad809ff3cdc43ec5
flufylele <ghostdenis@yahoo.com>
2019-03-21 21:18:38 +0200

    Coin system
    Added a sprite for the coins. Added the script for the coinpickup where it dissapears when the player collides with it. Added the script for player that when it collides with a coin it will increment the money vealue of the player with the value of the coined that got picked up. Added a bunch of coins on the scene to test it up.

---
+++ 2D MDS/Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour
+{
+    public float value;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if( collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 2D MDS/Assets/Scripts/PlayerMoney.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoney : MonoBehaviour
+{
+    private float money = 0;
+    private CoinPickUp coin;
+
+
+    private void Start()
+    {
+        coin = GameObject.FindGameObjectWithTag("Coin").GetComponent<CoinPickUp>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            money += coin.value;
+            Debug.Log(money);
+        }
+    }
+
+}
